4a8aa3685c86d345f365f3c106a126795eaa3207
James Kent <james-kent@uiowa.edu>
2018-05-08 23:33:03 -0500

    fix rec_ex bug

--- xnat_downloader/cli/run.py
+++ xnat_downloader/cli/run.py
@@ -289,6 +289,7 @@ class Subject:
             label = scan_pattern_dict[key]
             if label is not None:
                 if key == 'rec_ex':
+                    key = 'rec'
                     label = 'pu'
                 fname = '_'.join([fname, key + '-' + label])
 

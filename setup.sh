b9388290a1e3acbb9b70575115fddf6a71776063
Liza <elizabeth.levitis@mail.mcgill.ca>
2018-10-04 00:32:19 -0400

    added check for conda and simplified env creation

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
-conda create -n xnat_downloader_env python=2.7
-source activate xnat_downloader_env
-conda install -c conda-forge dcm2niix
-python setup.py
+if [ -z $(which conda) ]; then
+    echo "please install anaconda, see here: https://www.anaconda.com/download/"
+    exit 1
+fi
+conda create -n xnat_downloader_env python=2.7 dcm2niix -c conda-forge -y
+python setup.py install
 

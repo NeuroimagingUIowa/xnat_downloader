37943d0d52307be328a8537e763087ba0f5c1bfb
James Kent <jamesdkent21@gmail.com>
2025-10-15 12:03:24 -0500

    update build commands

--- scripts/build_dockerfile.sh
+++ scripts/build_dockerfile.sh
@@ -2,40 +2,43 @@
 
 # currently not working since shell is not an option
 generate_docker(){
-    docker run kaczmarj/neurodocker:0.7.0 generate docker \
-        --base=neurodebian:stretch-non-free \
+    docker run --rm repronim/neurodocker:2.0.0 generate docker \
+        --base-image=neurodebian:bookworm-non-free \
         --pkg-manager=apt \
         --env LANG=C.UTF-8 \
         --env LC_ALL=C.UTF-8 \
         --install pigz \
         --run "mkdir -p /opt/dcm2niix && \
             cd /opt/dcm2niix && \
-            curl -sSLO https://github.com/rordenlab/dcm2niix/releases/download/v1.0.20181125/dcm2niix_25-Nov-2018_lnx.zip && \
-            unzip dcm2niix_25-Nov-2018_lnx.zip && \
-            rm dcm2niix_25-Nov-2018_lnx.zip" \
+            curl -sSLO https://github.com/rordenlab/dcm2niix/releases/download/v1.0.20250506/dcm2niix_lnx.zip && \
+            unzip dcm2niix_lnx.zip && \
+            rm dcm2niix_lnx.zip" \
         --env 'PATH=/opt/dcm2niix:$PATH' \
         --user=coder \
         --copy . '/home/coder/project' \
         --user=root \
         --run "chown -R coder /home/coder/project" \
-        --user=coder \
         --workdir="/home/coder" \
         --env "SHELL=/bin/bash" \
+        --env CONDA_PLUGINS_AUTO_ACCEPT_TOS=true \
+        --user=root \
         --miniconda \
-            create_env='neuro' \
-            activate=true \
-            conda_install='python=2.7' \
+            method=binaries \
+            version=latest \
+            env_name=neuro \
+            env_exists=false \
+            conda_install='python=3.13' \
+        --user=coder \
         --run 'echo ". $CONDA_DIR/etc/profile.d/conda.sh" >> ~/.profile' \
-        --run "conda init bash" \
-        --run "conda activate neuro && pip install -e /home/coder/project/[test]" \
+        --run-bash 'source "${CONDA_DIR}/etc/profile.d/conda.sh" && conda activate neuro && pip install -e /home/coder/project/[test]' \
         --user=root \
         --entrypoint "/neurodocker/startup.sh xnat_downloader"
         # --shell "/bin/bash --login -c" \
 }
 
 generate_docker_devel(){
-    docker run kaczmarj/neurodocker:0.7.0 generate docker \
-    --base=jdkent/xnat_downloader:unstable \
+    docker run --rm repronim/neurodocker:2.0.0 generate docker \
+    --base-image=jdkent/xnat_downloader:unstable \
     --pkg-manager=apt \
     --user=coder \
     --workdir="/home/coder" \
@@ -43,7 +46,6 @@ generate_docker_devel(){
     --run "curl -o /tmp/code-server.tar.gz -SL https://github.com/cdr/code-server/releases/download/3.0.2/code-server-3.0.2-linux-x86_64.tar.gz" \
     --run "mkdir -p /opt/codeserver && tar -xvf /tmp/code-server.tar.gz -C /opt/codeserver --strip-components=1" \
     --run '/opt/codeserver/code-server --install-extension eamodio.gitlens && /opt/codeserver/code-server --install-extension ms-python.python' \
-    --expose 8080 \
     --entrypoint '/opt/codeserver/code-server --auth none --host 0.0.0.0 /home/coder/project'
 }
 
